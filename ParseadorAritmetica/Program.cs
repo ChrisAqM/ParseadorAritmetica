22c50acb06b9ff592b61b64adcfb1620b69c4c20
Christian <Christian@CHRIS_AQ>
2022-11-12 17:48:00 -0400

    Add project files.

---
+++ ParseadorAritmetica/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+namespace ParseadorAritmetica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "5*6+9*3-1";
+            Console.WriteLine($"Input: {input}\n");
+            ICharStream stream = CharStreams.fromString(input);
+            aritmeticaLexer lexer = new aritmeticaLexer(stream);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            aritmeticaParser parser = new aritmeticaParser(tokens);
+            aritmeticaParser.AritmeticaContext tree = parser.aritmetica();
+            Aritmetica aritmetica = new Aritmetica();
+            int res = aritmetica.Visit(tree);
+        }
+    }
+}
